fa02cd808242f3e6ae7cdec7ca9843712f7bcc4d
Pratiksha Pai <1312pratiksha@gmail.com>
2025-02-20 00:44:38 -0800

    Render Chess Board

---
+++ js/render.js
@@ -0,0 +1,28 @@
+const board = document.querySelector(".board");
+const initialBoard = [
+  ["r", "n", "b", "q", "k", "b", "n", "r"],
+  ["p", "p", "p", "p", "p", "p", "p", "p"],
+  [".", ".", ".", ".", ".", ".", ".", "."],
+  [".", ".", ".", ".", ".", ".", ".", "."],
+  [".", ".", ".", ".", ".", ".", ".", "."],
+  [".", ".", ".", ".", ".", ".", ".", "."],
+  ["P", "P", "P", "P", "P", "P", "P", "P"],
+  ["R", "N", "B", "Q", "K", "B", "N", "R"],
+];
+function renderBoard() {
+  board.innerHTML = "";
+  for (let i = 0; i < 8; i++) {
+    for (let j = 0; j < 8; j++) {
+      const square = document.createElement("div");
+      square.classList.add("square", (i + j) % 2 === 0 ? "light" : "dark");
+      square.dataset.row = i;
+      square.dataset.col = j;
+      if (initialBoard[i][j] !== ".") {
+        square.textContent = initialBoard[i][j];
+      }
+      board.appendChild(square);
+    }
+  }
+}
+
+renderBoard();
