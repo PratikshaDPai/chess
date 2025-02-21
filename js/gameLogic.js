fd4965da6936b1d370d503166dd614be9e6d29be
Pratiksha Pai <1312pratiksha@gmail.com>
2025-02-21 00:11:04 -0800

    Add isValidMove(), getOpponentTurn(),implement isCheck()

--- js/gameLogic.js
+++ js/gameLogic.js
@@ -33,8 +33,50 @@
 function isCheck(board, turn) {
   //for a board state, check that king of current turn is not checked
   //Step 1: find king's position
+  const opponentTurn = getOpponentTurn(turn);
   const king = turn === "B" ? "k" : "K";
   const kingCoordinate = getPieceCoordinate(board, king);
+  //Step 2: If not king, check if any opponent(non-king) piece has isValidMove() to king coordinate
+  //Iterate over board, find pieces opp of turn, check validmove(piece coordinate, king coordinate,board)
+  for (let x = 0; x < board.length; x++) {
+    for (let y = 0; y < board.length; y++) {
+      const piece = board[x][y];
+      if (piece === ".") continue;
+
+      // if turn= 'W' find lowercase non-king piece, if turn="B" find uppercase non-king piece
+      const nonKingPieces = {
+        W: ["R", "N", "B", "Q", "P"],
+        B: ["r", "n", "b", "q", "p"],
+      };
+      if (
+        nonKingPieces[opponentTurn].includes(piece) &&
+        isValidMove(board, { x, y }, kingCoordinate, opponentTurn)
+      ) {
+        //check if x,y to kingCoordinates is a validMove
+        return true;
+      }
+    }
+  }
+}
+
+/**
+ * @param {Turn} turn
+ * @returns {Turn}
+ */
+function getOpponentTurn(turn) {
+  return turn === "B" ? "W" : "B";
+}
+
+/**
+ *
+ * @param {Board} board
+ * @param {Coordinate} src
+ * @param {Coordinate} dest
+ * @param {Turn} turn
+ * @returns {boolean}
+ */
+function isValidMove(board, src, dest, turn) {
+  return true;
 }
 
 /**
