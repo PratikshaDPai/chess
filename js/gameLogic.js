8b787119619ba467a3665000491ab34cd74f7901
Pratiksha Pai <1312pratiksha@gmail.com>
2025-02-24 10:46:02 -0800

    Refactor code, add types.js to import types across all .js files

--- js/gameLogic.js
+++ js/gameLogic.js
@@ -1,33 +1,4 @@
 /**
- * @typedef {BoardSquare[][]} Board
- */
-
-/**
- * @typedef { 'R' | 'N' | "B" | "Q" | "K" | "P" | 'r' | 'n' | 'b' | 'q' | 'k' | 'p' } Piece
- */
-
-/**
- * @typedef {Piece|'.'} BoardSquare
- */
-
-/**
- * @typedef {'B'|'W'} Turn
- */
-
-/**
- *
- * @typedef Coordinate
- * @property {number} x
- * @property {number} y
- */
-
-/**
- * @typedef Move
- * @property {Coordinate} src
- * @property {Coordinate} dest
- */
-
-/**
  * Board level function. Needs access to
  * - Find king of current turn, check for
  * - For each opponent piece, is king’s square a valid move()?
